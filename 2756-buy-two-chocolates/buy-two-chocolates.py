7a033b46db0704d6318325ab6098e9bd460634f7
Ck <118044421+ckyong826@users.noreply.github.com>
2023-12-20 13:51:57 +0800

    Time: 67 ms (44.98%) | Memory: 16.3 MB (55.48%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -2,5 +2,5 @@ class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         prices.sort()
         temp=money
-        temp=temp-prices[0]-prices[1]
+        temp=temp-sum(prices[:2])
         return temp if temp>=0 else money
