888f0ab33b57e055752ece240dd212470a098c11
Ck <118044421+ckyong826@users.noreply.github.com>
2024-01-09 14:19:56 +0800

    Time: 49 ms (30.46%) | Memory: 17.5 MB (17.27%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,10 +1,3 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        temp=defaultdict(int)
-        for i in range(len(arr)):
-            temp[arr[i]] += 1
-        temp=sorted(temp.values())
-        for i in range(1,len(temp)):
-            if temp[i-1]==temp[i]:
-                return False
-        return True
+        return len(set(Counter(arr).values())) == len(Counter(arr).values())
