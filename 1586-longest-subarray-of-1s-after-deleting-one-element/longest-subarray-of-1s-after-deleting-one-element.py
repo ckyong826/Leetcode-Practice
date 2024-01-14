5d98b14f39ee1e514772b8971da1014abf6fb8cc
Ck <118044421+ckyong826@users.noreply.github.com>
2024-01-15 00:06:51 +0800

    Time: 429 ms (94.48%) | Memory: 21 MB (9.91%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,27 +1,14 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        maxLength=0
-        temp=0
-        count=0
-        index=[]
-        if sum(nums)==len(nums):
-            return len(nums)-1
-        for i in range(len(nums)):
-            if nums[i]==0 and count==2:
-                num=temp
-                temp-=sum(nums[index[-2]:index[-1]])
-                maxLength=max(maxLength,num,temp)
-                index.append(i)
-            elif nums[i]==0 and count==1:
-                maxLength=temp
-                temp-=sum(nums[:index[-1]])
-                maxLength=max(maxLength,temp)
-                count+=1
-                index.append(i)
-            elif nums[i]==0:
-                count+=1
-                index.append(i)
+        n = len(nums)
+        ans,cnt,prev=0,0,-1
+        for i in range(n):
+            if nums[i]==0:
+                ans=max(ans,cnt)
+                cnt=i-prev-1
+                prev=i
             else:
-                temp+=nums[i]
-        maxLength=max(maxLength,temp)
-        return maxLength
+                cnt+=1
+        ans=max(cnt,ans)
+        return ans-1 if ans==n else ans
+
