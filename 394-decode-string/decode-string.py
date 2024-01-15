c1bfb7078c4d09838f8d13f6e9e3c10128f7a503
Ck <118044421+ckyong826@users.noreply.github.com>
2024-01-15 22:47:58 +0800

    Time: 30 ms (93.01%) | Memory: 17.4 MB (16.22%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        num,temp,ans,result="","","",""
+        stack=[]
+        for stg in s:
+            if stg.isdigit():
+                num+=stg
+            elif stg.isalpha():
+                temp+=stg
+            elif stg=="[":
+                stack+=[(int(num),temp)]
+                temp,num="",""
+            elif stg=="]":
+                prev_num,prev_txt=stack.pop()
+                temp=prev_txt+prev_num*temp
+        return temp
