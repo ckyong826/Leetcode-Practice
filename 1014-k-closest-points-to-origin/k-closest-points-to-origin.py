5f48d61b4a5e42cf8fbc19ee7db4b6e58a06ccec
Ck <118044421+ckyong826@users.noreply.github.com>
2024-07-22 21:25:28 +0800

    Time: 646 ms (15.26%) | Memory: 23.2 MB (15.30%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,5 +3,8 @@ class Solution:
         toD = lambda x,y : math.sqrt(pow((x-0),2)+pow(y-0,2))
         heap = [(toD(x,y),[x,y]) for x,y in points] 
         heapq.heapify(heap)
-        res=[heapq.heappop(heap)[1] for n in range(k)]
+        res=[]
+        for i in range(k):
+            a,b = heapq.heappop(heap)
+            res.append(b)
         return res
