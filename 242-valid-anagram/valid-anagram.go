988333d4d8464aface7e6191c0c77d91a562ccb4
Ck <118044421+ckyong826@users.noreply.github.com>
2025-02-08 01:44:52 +0800

    Time: 4 ms (58.51%) | Memory: 4.8 MB (73.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -2,18 +2,16 @@ func isAnagram(s string, t string) bool {
     if (len(s)!=len(t)){
         return false
     }
-    setS := make(map[rune]int)
-    setT := make(map[rune]int)
+    set := map[rune]int{}
     for _,str :=range s{
-        setS[str] ++
+        set[str] ++
     }
     for _,str :=range t{
-        setT[str] ++
-    }
-    for key,val := range setS {
-        if val!=setT[key]{
-            return false
+        set[str] --
+        if(set[str]==0){
+            delete(set,str)
         }
     }
-    return true
+    
+    return len(set)==0
 }
