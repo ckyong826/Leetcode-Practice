b2e42d826b18b62e44c463668c036c22684e4006
Ck <118044421+ckyong826@users.noreply.github.com>
2025-02-09 22:58:56 +0800

    Time: 1 ms (87.58%) | Memory: 9.5 MB (31.33%) - LeetSync

--- 155-min-stack/min-stack.go
+++ 155-min-stack/min-stack.go
@@ -1,31 +1,45 @@
 type MinStack struct {
     stack []int
-    min int
+    min []int
+    top int
 }
 
 
 func Constructor() MinStack {
-    return MinStack{}
+    return MinStack{[]int{},[]int{},-1}
 }
 
 
 func (this *MinStack) Push(val int)  {
+    if this.top == -1 {
+        this.min = append(this.min,val)
+    } else {
+        this.min = append(this.min, min(val,this.min[this.top]))
+    }
     this.stack = append(this.stack,val)
+    this.top++
 }
 
 
 func (this *MinStack) Pop()  {
-    this.stack = this.stack[:len(this.stack)-1]
+    this.stack = this.stack[:this.top]
+    this.min = this.min[:this.top]
+    this.top--
 }
 
 
 func (this *MinStack) Top() int {
-    return this.stack[len(this.stack)-1]
+    return this.stack[this.top]
 }
 
 
 func (this *MinStack) GetMin() int {
-    return slices.Min(this.stack)
+    return this.min[this.top]
+}
+
+func min(a,b int) int {
+    if a<b {return a}
+    return b
 }
 
 
