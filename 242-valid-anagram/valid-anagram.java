30d0964c5b904698bf82cc42b01f6c388a4ad216
Ck <118044421+ckyong826@users.noreply.github.com>
2025-05-17 14:42:59 +0800

    Time: 4 ms (77.07%) | Memory: 44.7 MB (35.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // Solution 1 : HashMap
         if (s.length()!=t.length()){
             return false;
         }
-        HashMap<Character,Integer> sCount = new HashMap<>();
-        HashMap<Character,Integer> tCount = new HashMap<>();
+        // Solution 1 : HashMap
+        // HashMap<Character,Integer> sCount = new HashMap<>();
+        // HashMap<Character,Integer> tCount = new HashMap<>();
+        // for (int i=0;i<s.length();i++){
+        //     sCount.put(s.charAt(i),sCount.getOrDefault(s.charAt(i),0)+1);
+        //     tCount.put(t.charAt(i),tCount.getOrDefault(t.charAt(i),0)+1);
+        // }
+        // return sCount.equals(tCount);
+
+        // Solution 2 : Sort
+        char[] sortedS = s.toCharArray();
+        Arrays.sort(sortedS);
+        char[] sortedT = t.toCharArray();
+        Arrays.sort(sortedT);
         for (int i=0;i<s.length();i++){
-            sCount.put(s.charAt(i),sCount.getOrDefault(s.charAt(i),0)+1);
-            tCount.put(t.charAt(i),tCount.getOrDefault(t.charAt(i),0)+1);
+            if (sortedS[i]!=sortedT[i]){
+                return false;
+            }
         }
-        return sCount.equals(tCount);
+        return true;
     }
 }
